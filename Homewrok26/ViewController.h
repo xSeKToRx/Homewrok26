f8f8ae84f3ab0fee75229bd9673e5b2f1ea5123e
xSeKToRx <dimasektor@Dimas-MacBook-Pro.local>
2016-08-04 15:24:39 +0300

    !

--- Homewrok26/ViewController.h
+++ Homewrok26/ViewController.h
@@ -9,7 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *view2;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UISlider *speedSlider;
+@property (weak, nonatomic) IBOutlet UISwitch *translationSwitch;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *imageSegmentedControl;
+@property (weak, nonatomic) IBOutlet UISwitch *rotationSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *scaleSwitch;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
 
+
+- (IBAction)imageChanged:(UISegmentedControl *)sender;
+- (IBAction)valueChanged:(UISlider *)sender;
+- (IBAction)actionTranslationSwitch:(UISwitch *)sender;
+- (IBAction)actionScaleSwitch:(UISwitch *)sender;
+- (IBAction)actionRotationSwitch:(UISwitch *)sender;
 
 @end
 
--- Homewrok26/ViewController.m
+++ Homewrok26/ViewController.m
@@ -10,18 +10,92 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setImage];
+  }
+
+#pragma mark - Actions
+- (IBAction)valueChanged:(UISlider *)sender {
+    self.sliderLabel.text=[NSString stringWithFormat:@"Moving speed : %1.1f ",self.speedSlider.value];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)actionTranslationSwitch:(UISwitch *)sender {
+  [self makeAnimations];
+
 }
+
+- (IBAction)actionScaleSwitch:(UISwitch *)sender {
+    [self makeAnimations];
+
+}
+
+- (IBAction)actionRotationSwitch:(UISwitch *)sender {
+    [self makeAnimations];
+
+   
+}
+- (IBAction)imageChanged:(UISegmentedControl *)sender {
+    self.imageView.backgroundColor=[UIColor clearColor];
+    [self setImage];
+}
+#pragma mark - Methods
+
+-(void)setImage{
+    self.imageView.image=[UIImage imageNamed:[NSString stringWithFormat:@"%d",self.imageSegmentedControl.selectedSegmentIndex]];
+}
+
+-(void)makeAnimations{
+    CGRect rect = self.view2.bounds;
+    
+    CGFloat x = arc4random() % (int)CGRectGetWidth(rect)+ CGRectGetMinX(rect);
+    CGFloat y = arc4random() % (int)CGRectGetHeight(rect) + CGRectGetMinY(rect);
+    
+    CGFloat s = (float)(arc4random() % 151) / 100.f + 0.5f;
+    
+    CGFloat r = (float)(arc4random() % (int)(M_PI * 2*10000)) / 10000 - M_PI;
+    
+    CGFloat d = 1/self.speedSlider.value;
+    CGPoint center=self.imageView.center;
+    CGAffineTransform scale;
+    CGAffineTransform rotation;
+    
+    if (self.translationSwitch.isOn==YES) {
+        center = CGPointMake(x, y);
+    }
+    if (self.scaleSwitch.isOn==YES) {
+        scale = CGAffineTransformMakeScale(s, s);
+    }
+    if (self.rotationSwitch.isOn==YES) {
+        rotation = CGAffineTransformMakeRotation(r);
+    }
+
+    [UIImageView animateWithDuration:d
+                          delay:0
+                        options:UIViewAnimationOptionCurveLinear
+                     animations:^{
+                         self.imageView.center=center;
+                         if (self.scaleSwitch.isOn==YES) {
+                             self.imageView.transform = CGAffineTransformMakeScale(s, s);
+                         }
+                         if (self.rotationSwitch.isOn==YES) {
+                             self.imageView.transform = CGAffineTransformMakeRotation(r);
+                         }
+                          if (self.scaleSwitch.isOn==YES &&self.rotationSwitch.isOn==YES) {
+                              self.imageView.transform=CGAffineTransformConcat(scale, rotation);
+                          }
+                     }
+                     completion:^(BOOL finished) {
+                         [self makeAnimations];
+                         
+                     }];
+}
+
+
+
 
 @end
